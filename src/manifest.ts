1b4dda7c4f2698c4429e876d5ee34887f5d6ef40
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-01-23 00:58:09 +0100

    add icons for extension

--- src/manifest.ts
+++ src/manifest.ts
@@ -17,13 +17,13 @@ const manifest = defineManifest(async () => ({
   background: { service_worker: "src/pages/background/index.ts" },
   action: {
     default_popup: "src/pages/popup/index.html",
-    default_icon: "icons/34x34.png",
+    default_icon: "icons/icon-32.png",
   },
   // chrome_url_overrides: {
   //   newtab: "src/pages/newtab/index.html",
   // },
   icons: {
-    "128": "icons/128x128.png",
+    "128": "icons/icon-128.png",
   },
   content_scripts: [
     {
