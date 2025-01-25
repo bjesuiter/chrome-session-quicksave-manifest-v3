434965a60aaad97299eebedaa711d8993ef5e71b
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-01-25 22:14:41 +0100

    fix vite config dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -10,53 +10,57 @@ const assetsDir = resolve(root, "assets");
 const outDir = resolve(__dirname, "dist");
 const publicDir = resolve(__dirname, "public");
 
-const isDev = process.env.__DEV__ === "true";
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
+  const isProd = mode === "production";
 
-if (isDev) {
-  console.info("DEV MODE IS ACTIVE!");
-}
+  if (isDev) {
+    console.log("DEV BUILD ACTIVE!");
+  }
 
-export default defineConfig({
-  plugins: [solidPlugin(), crx({ manifest })],
-  resolve: {
-    alias: {
-      "@src": root,
-      "@assets": assetsDir,
-      "@pages": pagesDir,
-    },
-  },
-  publicDir,
-  build: {
-    outDir,
-    sourcemap: isDev,
-    minify: !isDev,
-    rollupOptions: {
-      // input: {
-      //   devtools: resolve(pagesDir, "devtools", "index.html"),
-      //   panel: resolve(pagesDir, "panel", "index.html"),
-      //   content: resolve(pagesDir, "content", "index.ts"),
-      //   background: resolve(pagesDir, "background", "index.ts"),
-      //   contentStyle: resolve(pagesDir, "content", "style.scss"),
-      //   popup: resolve(pagesDir, "popup", "index.html"),
-      //   newtab: resolve(pagesDir, "newtab", "index.html"),
-      //   options: resolve(pagesDir, "options", "index.html"),
-      // },
-      output: {
-        //   entryFileNames: "src/pages/[name]/index.js",
-        chunkFileNames: isDev
-          ? "assets/js/[name].js"
-          : "assets/js/[name].[hash].js",
-        //   assetFileNames: (assetInfo) => {
-        //     const { dir, name: _name } = path.parse(assetInfo.name);
-        //     // const assetFolder = getLastElement(dir.split("/"));
-        //     // const name = assetFolder + firstUpperCase(_name);
-        //     return `assets/[ext]/${name}.chunk.[ext]`;
+  return {
+    plugins: [solidPlugin(), crx({ manifest })],
+    resolve: {
+      alias: {
+        "@src": root,
+        "@assets": assetsDir,
+        "@pages": pagesDir,
       },
-      // },
     },
-  },
-  // vitest config
-  // test: {
+    publicDir,
+    build: {
+      outDir,
+      sourcemap: isDev,
+      minify: isProd,
+      rollupOptions: {
+        // input: {
+        //   devtools: resolve(pagesDir, "devtools", "index.html"),
+        //   panel: resolve(pagesDir, "panel", "index.html"),
+        //   content: resolve(pagesDir, "content", "index.ts"),
+        //   background: resolve(pagesDir, "background", "index.ts"),
+        //   contentStyle: resolve(pagesDir, "content", "style.scss"),
+        //   popup: resolve(pagesDir, "popup", "index.html"),
+        //   newtab: resolve(pagesDir, "newtab", "index.html"),
+        //   options: resolve(pagesDir, "options", "index.html"),
+        // },
+        output: {
+          //   entryFileNames: "src/pages/[name]/index.js",
+          // preserveModules: isDev,
+          chunkFileNames: isDev
+            ? "assets/js/[name].js"
+            : "assets/js/[name].[hash].js",
+          //   assetFileNames: (assetInfo) => {
+          //     const { dir, name: _name } = path.parse(assetInfo.name);
+          //     // const assetFolder = getLastElement(dir.split("/"));
+          //     // const name = assetFolder + firstUpperCase(_name);
+          //     return `assets/[ext]/${name}.chunk.[ext]`;
+        },
+        // },
+      },
+    },
+    // vitest config
+    // test: {
 
-  // }
+    // }
+  };
 });
