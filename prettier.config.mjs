108696e7648b1587b6c1da067454673e4a998ab1
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-01-25 21:31:17 +0100

    update prettier config

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -2,13 +2,11 @@
 
 import * as prettierPluginTailwind from "prettier-plugin-tailwindcss";
 
-const config = {
+export default {
   // prettierPluginTailwind auto-detects: tailwindConfig: './tailwind.config.js',
   plugins: [prettierPluginTailwind],
   // default: trailingComma: "es5",
   singleQuote: false,
   requirePragma: false,
   arrowParens: "always",
-};
-
-export default config;
+}
