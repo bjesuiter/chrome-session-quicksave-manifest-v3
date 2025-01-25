cb199db7cc2fb3492ceb03afa78ae1bfd307d11d
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-01-25 18:13:00 +0100

    delete content script and panel pages (not needed)

--- src/pages/content/index.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from "solid-js/web";
-import { App } from "./components/Demo/app";
-
-const root = document.createElement("div");
-root.id = "my-extension-root";
-document.body.append(root);
-
-render(App, root);
