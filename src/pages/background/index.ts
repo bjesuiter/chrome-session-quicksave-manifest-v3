ce2722a14c6caa32c64afe97801a04c4915e081a
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-01-25 18:46:36 +0100

    add chrome.runtime.onInstalled listener

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,1 +1,17 @@
-console.log("background loaded");
+import { showError } from "@src/lib/chrome-services/notification-service";
+import { initializeOptions } from "@src/lib/main/initialize-options";
+
+console.log("background service worker loaded");
+
+// Code to run when extension gets installed
+chrome.runtime.onInstalled.addListener(async function () {
+    try {
+        await initializeOptions();
+        console.log("Extension Installed Successfully!");
+    } catch (error) {
+        const errorMessage =
+            `Error while creating Extension Options Storage! Please contact the developer about it!`;
+        showError("Installation Error", errorMessage);
+        console.error(errorMessage, error);
+    }
+});
