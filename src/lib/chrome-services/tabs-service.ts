d5335c887e769261d4aa460e5d1272fbcca16f00
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-01-25 18:43:00 +0100

    add tabs service

---
+++ src/lib/chrome-services/tabs-service.ts
@@ -0,0 +1,15 @@
+/**
+ *
+ * Chrome Docs for chrome.tabs.query: https://developer.chrome.com/extensions/tabs#method-query
+ * Chrome Docs for chrome.tabs.Tab Type: https://developer.chrome.com/extensions/tabs#type-Tab
+ * TODO: Check how to deal with Tabs sleeping from 'The Great Suspender' Extension
+ * @returns {Promise<Array<chrome.tabs.Tab>>}
+ */
+export async function getTabsInWindow(windowId: number): Promise<Array<chrome.tabs.Tab>> {
+	return new Promise(resolve => {
+		const queryInfo: chrome.tabs.QueryInfo = {
+			windowId
+		};
+		chrome.tabs.query(queryInfo, resolve);
+	});
+}
