41be90d56c2cc3871f580681fbecf0494cb4c7cb
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-01-25 21:43:53 +0100

    fix prettier config

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -5,13 +5,12 @@ console.log("background service worker loaded");
 
 // Code to run when extension gets installed
 chrome.runtime.onInstalled.addListener(async function () {
-    try {
-        await initializeOptions();
-        console.log("Extension Installed Successfully!");
-    } catch (error) {
-        const errorMessage =
-            `Error while creating Extension Options Storage! Please contact the developer about it!`;
-        showError("Installation Error", errorMessage);
-        console.error(errorMessage, error);
-    }
+  try {
+    await initializeOptions();
+    console.log("Extension Installed Successfully!");
+  } catch (error) {
+    const errorMessage = `Error while creating Extension Options Storage! Please contact the developer about it!`;
+    showError("Installation Error", errorMessage);
+    console.error(errorMessage, error);
+  }
 });
