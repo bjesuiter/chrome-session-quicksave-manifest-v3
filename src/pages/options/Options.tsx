07ecb08898bdb9ad53a1f089bdcf59961fb20eaa
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-01-26 00:25:38 +0100

    add basic options page structure

--- src/pages/options/Options.tsx
+++ src/pages/options/Options.tsx
@@ -1,22 +1,27 @@
-import logoDataUrl from "@src/assets/img/logo.svg";
+import { isDev } from "@src/lib/flags";
 import "@src/styles/tailwind.css";
+import { Show } from "solid-js";
 
 export function OptionsPage() {
   return (
-    <div class="fixed inset-0 bg-[#282c34] text-white">
-      <h1 class="text-xl font-bold">Options Page Template</h1>
-      <section class="top-24 mx-auto w-fit p-5">
-        {/* get urls for imported assets: chrome.runtime.getURL(), but NOT for images, images are inlined as data urls from vite 5 on! */}
-        <img
-          src={logoDataUrl}
-          class="motion-preset-spin pointer-events-none max-w-80"
-          alt="logo"
-        />
-        <p class="flex flex-wrap pt-10 text-base font-bold">
-          Edit &nbsp; <code>src/pages/options/Options.tsx</code> &nbsp; and save
-          to reload.
-        </p>
-      </section>
+    <div class="fixed inset-0 bg-slate-100 text-slate-800">
+      <div class="mx-auto my-10 min-w-[300px] max-w-[900px] overflow-hidden rounded border-2 border-slate-200">
+        <header class="flex flex-row items-center gap-4 bg-slate-800 p-5 text-white">
+          <img
+            class="size-12"
+            src={chrome.runtime.getURL("icons/icon-512.png")}
+          />
+          <h1 class="text-2xl font-semibold">Session Quicksave Options</h1>
+        </header>
+
+        {/* Content Section */}
+        <section class="min-h-[150px] overflow-y-auto p-5">
+          <Show when={isDev}>
+            <h2 class="my-2 text-xl font-bold text-red-600">DEV Section</h2>
+            <div class="border-b-2 border-dashed border-red-600" />
+          </Show>
+        </section>
+      </div>
     </div>
   );
 }
